cbcb4e128b2df7aaa0c19e5361f47452a2e75460
Keisuke Hata <keisuke@hata.biz>
2013-11-17 02:32:10 +0900

    remove

--- CCBReader/jsbindings/src/manual/js_bindings_CCControl_registration.h
+++
@@ -1,7 +0,0 @@
-#ifndef __JSB_CCCONTROL_REGISTRATION
-#define __JSB_CCCONTROL_REGISTRATION
-
-void jsb_register_CCControl( JSContext *_cx, JSObject *globalO);
-
-
-#endif
--- CCBReader/jsbindings/src/manual/js_bindings_CCScrollView_registration.h
+++
@@ -1,7 +0,0 @@
-#ifndef __JSB_CCSCROLLVIEW_REGISTRATION
-#define __JSB_CCSCROLLVIEW_REGISTRATION
-
-void jsb_register_CCScrollView( JSContext *_cx, JSObject *globalO);
-
-
-#endif
